aed610bb35e8da3571c56e3351b9576b063c7cf1
Dennis F. <setchring@users.noreply.github.com>
2024-09-04 11:29:48 +0200

    Update __init__.py

--- custom_components/tibber_custom/__init__.py
+++ custom_components/tibber_custom/__init__.py
@@ -3,11 +3,13 @@ import logging
 
 from homeassistant.const import EVENT_HOMEASSISTANT_START
 from homeassistant.helpers import discovery
+from .const import (
+    COMPONENT_DOMAIN,
+    DEPENDENCIES
+)
 
 
-DOMAIN = "tibber_custom"
 
-DEPENDENCIES = ["tibber"]
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -16,7 +18,7 @@ def setup(hass, config):
     """Setup component."""
 
     def ha_started(_):
-        discovery.load_platform(hass, "camera", DOMAIN, {}, config)
+        discovery.load_platform(hass, "camera", COMPONENT_DOMAIN, {}, config)
 
     hass.bus.listen_once(EVENT_HOMEASSISTANT_START, ha_started)
 
