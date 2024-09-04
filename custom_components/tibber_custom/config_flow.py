83441376c005ce0ae8dd18d2e54a5ca12162d4e5
Dennis F. <setchring@users.noreply.github.com>
2024-09-04 12:01:14 +0200

    Update config_flow.py

--- custom_components/tibber_custom/config_flow.py
+++ custom_components/tibber_custom/config_flow.py
@@ -1,10 +1,14 @@
 from homeassistant import config_entries
-from .const import DOMAIN
+from .const import COMPONENT_DOMAIN
 
 
-class ExampleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Example config flow."""
+@config_entries.HANDLERS.register(COMPONENT_DOMAIN)
+class TibberCustomFlow(config_entries.ConfigFlow, domain=COMPONENT_DOMAIN):
+    """Tibber Custom config flow."""
     # The schema version of the entries that it creates
     # Home Assistant will call your migrate method if the version changes
     VERSION = 1
     MINOR_VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        """Handle user step."""
